81cc2a61eb6fcf1b7301453b65233635647800d3
Alan Shaw <alan@freestyle-developments.co.uk>
2013-07-24 09:07:35 +0100

    Fixes #2

--- test/expected/all.js
+++ test/expected/all.js
@@ -2,4 +2,5 @@
 	window.tmpl=window.tmpl||{};
 	tmpl['bar']=Hogan.compile('<div class="something"><ul>{{#foo}}<li><h1>{{bar}}</h1></li>{{/foo}}</ul></div>');
 	tmpl['foo']=Hogan.compile('<div class="something"><ul>{{#foo}}<li><h1>{{bar}}</h1></li>{{/foo}}</ul></div>');
+	tmpl['val']=Hogan.compile('please {{compile}} me');
 }());
